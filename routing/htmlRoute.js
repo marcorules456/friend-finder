882eb76eae0cd29df030b2403d8c225fb8e87773
Marco Khodr <marcokhodr16@gmail.com>
2017-10-21 13:08:06 -0700

    l

--- routing/htmlRoute.js
+++ routing/htmlRoute.js
@@ -3,30 +3,24 @@
 var express  = require("express");
 var app = express();
 app.listen(3000);
+var path = require('path');
 
 
 
+module.exports = function(app) {
+
+	app.get('/survey', function(req, res){
+
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+
+	})
 
 
 
+	app.use(function(req, res){
 
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
 
-
-
-
-function htmlRoute(){
-
-app.get("/home",function(req,res){
-
-	res.sendFile("../public/home.html");
-});
-
-
-app.get("/survey",function(req,res){
-
-	res.sendFile("../public/survey.html");
-});
+	});
 
 }
-
-module.exports = htmlRoute;
